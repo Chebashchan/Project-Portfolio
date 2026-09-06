52ea75e188b53cc1eb5e9333c4e09a1c51b4e8bb
Chebashchan <idukesebastian@gmail.com>
2026-09-06 18:33:12 +0800

    fixed import

--- app/projects/[slug]/page.jsx
+++ app/projects/[slug]/page.jsx
@@ -1,7 +1,9 @@
 import { getProjectBySlug } from '../../../lib/data/projects'
 import { notFound } from 'next/navigation'
 import Link from 'next/link'
-import ProjectCarousel from '../../../components/projects/ProjectCarousel'
+// Change line 4 to this:
+import ProjectCarousel from '@/components/projects/ProjectCarousel'
+
 
 export default async function ProjectDetailPage({ params }) {
   const { slug } = await params
